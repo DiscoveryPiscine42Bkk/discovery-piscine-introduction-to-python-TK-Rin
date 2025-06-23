45b0019be47f1c8581553e8f4b9b9794dffc3d80
TK-Rin <thiradet.kur@gmail.com>
2025-06-23 14:16:38 +0000

    .

---
+++ cell04/ex01/age.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+x = int(input("Please tell me your age: "))
+print("You are currently", x, "years old.")
+print("In 10 years, you'll be", x + 10, "years old.")
+print("In 20 years, you'll be", x + 20, "years old.")
+print("In 30 years, you'll be", x + 30, "years old.")
---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+x = int(input("Give me the first number: "))
+y = int(input("Give me the second number: "))
+a = x+y
+b = x-y
+c = x/y
+d = x*y
+print("Thank you!")
+print(x, "+", y, "=", a)
+print(x, "-", y, "=", b)
+print(x, "/", y, "=", int(c))
+print(x, "*", y, "=", d)
